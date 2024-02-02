e84f571b477ad58c4f54fe32bc00e94472893287
Koundu <koundinya.g07@gmail.com>
2024-02-02 14:02:30 +0000

    [LeetCode Sync] Runtime - 48 ms (3.04%), Memory - 48.9 MB (70.34%)

---
+++ my-folder/0066-plus-one/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    const n = digits.length;
+    for(let i=n-1;i>=0;i--){
+        digits[i]++;
+        if(digits[i]<10){
+            return digits;
+        }
+        digits[i]=0;
+    }
+    digits.unshift(1)
+    return digits;
+};
